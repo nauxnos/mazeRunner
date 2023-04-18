639d3d5c37c07b420798d100ca089b883d3d0134
nosnaux <son942002@gmail.com>
2023-04-18 13:10:20 +0700

    add turn_around()

--- control.c
+++ control.c
@@ -1,4 +1,5 @@
 #include <reg51.h>
+#include <sensor.h>
 
 sbit left_1 = P1^0;
 sbit left_2 = P1^1;
@@ -16,17 +17,19 @@ void straight()
 void left()
 {
     left_1 = 1;
-    left_2 = 1;
+    left_2 = 0;
     right_1 = 1;
     right_2 = 0;
+    // delay(10000);
 }
 
 void right()
 {
     left_1 = 0;
     left_2 = 1;
-    right_1 = 1;
+    right_1 = 0;
     right_2 = 1;
+    // delay(10000);
 }
 
 void stop()
@@ -35,4 +38,9 @@ void stop()
     left_2 = 1;
     right_1 = 1;
     right_2 = 1;
+}
+
+void turn_around()
+{
+    right();
 }
