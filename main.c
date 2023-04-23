61599c1d6948dfb902c92c8336bfbd947dcc966c
nosnaux <son942002@gmail.com>
2023-04-23 20:37:17 +0700

    update turn right

--- main.c
+++ main.c
@@ -6,21 +6,23 @@ void main()
 {
 	while (1)
 	{
-		if(!space_right()) {
-			right();
-		}
+		if(space_front()) stop();
 		else {
-			if (!space_front()) {
+			if(!space_right()) {
+				right();
+				delay(100000);
+				delay(100000);
+				delay(15000);
+				stop();
+				delay(100000);
+				delay(100000);
+				delay(8000);
 				straight();
+				while(!space_right());
 			}
 			else {
-				if(!space_left()) {
-					left();
-				}
-				else {
-					stop();
-				}
+				straight();
 			}
 		}
 	}
-} // 1020
+} // 1019
